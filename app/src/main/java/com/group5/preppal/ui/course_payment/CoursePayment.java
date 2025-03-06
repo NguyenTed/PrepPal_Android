1b1e1e5cc27a3e84460b5da0cdb2ec6438613ffd
huy37204 <huy37204@gmail.com>
2025-03-06 22:42:18 +0700

    finish design ui for course register

---
+++ app/src/main/java/com/group5/preppal/ui/course_payment/CoursePayment.java
@@ -0,0 +1,16 @@
+package com.group5.preppal.ui.course_payment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.group5.preppal.R;
+
+public class CoursePayment extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_payment);
+    }
+}
