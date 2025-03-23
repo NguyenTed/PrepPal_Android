c41af98fe5355b2314517d5b265febfca6ba71df
huy37204 <huy37204@gmail.com>
2025-03-23 22:17:47 +0700

    nothing

--- app/src/main/java/com/group5/preppal/ui/auth/LoginActivity.java
+++ app/src/main/java/com/group5/preppal/ui/auth/LoginActivity.java
@@ -132,8 +132,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
